e487d4f111c0ee4daf0d2f94f8b1c6cf4b64a4ee
Qendresa Shala <alesa@live.se>
2023-01-10 15:13:17 +0100

    Changed classes

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+type Scientist = {
+    name: string,
+    age: number,
+    henchmen: number,
+    description: string
+}
+
+const listOfScientists: Scientist[] = [
+    {
+        name: "Mr Old",
+        age: 300,
+        henchmen: 2,
+        description: "Old Evil Scientist who is young at heart."
+
+    },
+    {
+        name: "Jim",
+        age: 30,
+        henchmen: 6,
+        description: "Likes to improvise"
+
+    },
+    {
+        name: "Jan",
+        age: 20,
+        henchmen: 8,
+        description: "Enjoys yoga besides being evil"
+
+    },
+];
+
+
