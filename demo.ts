477f51e3da82139b0d7e06fa81ca33d7b1ddf33e
Qendresa Shala <alesa@live.se>
2023-01-12 15:10:14 +0100

    New TS code

---
+++ demo.ts
@@ -0,0 +1,21 @@
+
+// const divElement = document.getElementsByClassName("horse")[0];
+// divElement.append("horse");
+
+// const horseElement = document.querySelector(".horse") as HTMLElement;
+// horseElement?.append("horse again");
+// // //good practice
+
+
+
+// const inputElement = document.querySelector("input") as HTMLInputElement;
+// const buttonElement = document.querySelector("button") as HTMLButtonElement;
+
+// buttonElement.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // console.log("click");
+
+//     horseElement.append(inputElement.value);
+//     inputElement.value = "";
+
+// });
--- src/app.ts
+++ src/app.ts
@@ -2,40 +2,91 @@ type Scientist = {
     nickname: string,
     age: number,
     henchmen: number,
-    description: string
+    description: string,
 }
 
-const listOfScientists: Scientist[] = [
+const allScientists: Scientist[] = [
     {
-        nickname: "Mr Young",
+        nickname: "Muffin",
         age: 300,
         henchmen: 2,
-        description: "Old Evil Scientist who is young at heart."
+        description: "An Old Evil Scientist with a young heart."
 
     },
     {
-        nickname: "Jim",
+        nickname: "Jinglebell",
         age: 30,
         henchmen: 6,
-        description: "Likes to improvise"
+        description: "Likes to improvise with evil stuff"
 
     },
     {
-        nickname: "Jan",
+        nickname: "Truffle",
         age: 20,
         henchmen: 8,
-        description: "Enjoys yoga and trees besides being evil"
+        description: "Enjoys yoga and trees"
 
     },
+
 ]
 
 
-// const allScientists = document.getElementById("allScientists") as HTMLDivElement;
 
-// const nameInput = document.querySelector('name-input') as HTMLInputElement;
-// const ageInput = document.querySelector('age-input') as HTMLInputElement;
-// const henchInput = document.querySelector('hench-input') as HTMLInputElement;
-// const infoInput = document.querySelector('infoInput') as HTMLInputElement;
+// a function to show all Evil Scientists from the array 
+
+const showScientists = document.getElementById('show-scientists') as HTMLElement;
+const detailedScientist = document.getElementById('detailed-scientist') as HTMLElement;
+const detailedText = document.getElementById('detailed-text') as HTMLParagraphElement;
 
 
+function printScientists() {
 
+    for (let i = 0; i < length; i++) {
+        const createCard = document.createElement("div") as HTMLDivElement;
+        createCard.className = "card";
+        createCard.innerHTML = `${allScientists[i].nickname}`;
+        const cardButton = document.createElement('button');
+        cardButton.className = "showScientists";
+        cardButton.innerHTML = "About";
+        showScientists.append(createCard);
+        showScientists.append(cardButton);
+
+    }
+
+
+    //listener when clicking on the nickname
+    const aboutButton = document.getElementsByClassName("showScientists");
+
+    for (let s = 0; s < aboutButton.length; s++) {
+        aboutButton[s].addEventListener("click", function () {
+            detailedText.innerHTML = `${allScientists[s].description}`
+
+        })
+    }
+
+}
+
+// a function to add a new scientist
+
+const buttonAdd = document.getElementById('button-add') as HTMLInputElement;
+const nameInput = document.getElementById('name-input') as HTMLInputElement;
+const ageInput = document.getElementById('age-input') as HTMLInputElement;
+const henchInput = document.getElementById('hench-input') as HTMLInputElement;
+const infoInput = document.getElementById('info-input') as HTMLInputElement;
+
+buttonAdd.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const newScientist: Scientist = {
+        nickname: nameInput.value,
+        age: parseInt(ageInput.value),
+        henchmen: parseInt(henchInput.value),
+        description: infoInput.value,
+    }
+
+    allScientists.push(newScientist);
+    showScientists.innerHTML = "";
+    printScientists();
+});
+
+printScientists();
